c087b38a4c9b83ce83296f237504c6fad5375e55
Vajith Chamuditha <vajithc@gmail.com>
2021-05-22 00:38:38 +0530

    Responsive fix

--- src/components/my-carousal/my-carousal.component.jsx
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -4,12 +4,11 @@ import Slide1 from "../../assets/img/carousal/pexels-photo-4.jpg";
 import Slide2 from "../../assets/img/carousal/pexels-photo-5.jpg";
 import Slide3 from "../../assets/img/carousal/slide2.webp";
 import ScrollDown from "../scroll-down/scroll-down.component";
-import Container from 'react-bootstrap/Card'
 import "./my-carousal.styles.css";
 
 const MyCarousal = () => {
   return (
-    <Container id="home">
+    <div id="home">
       <Carousel controls={false} indicators interval={2500} pause={false}>
         <Carousel.Item>
           <img className="d-block w-100 custom-img" src={Slide2} alt="First slide" />
@@ -22,7 +21,7 @@ const MyCarousal = () => {
         </Carousel.Item>
       </Carousel>
       <ScrollDown />
-    </Container>
+    </div>
   );
 };
 

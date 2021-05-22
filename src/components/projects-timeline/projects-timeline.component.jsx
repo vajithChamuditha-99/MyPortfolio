9892ae8003de03b0420aa6ce5452d63cc09703f2
Vajith Chamuditha <vajithc@gmail.com>
2021-05-22 12:27:27 +0530

    UI modified

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -51,11 +51,8 @@ const TimeLine = () => {
                   <Card.Footer className="text-muted">
                     <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
                       <Button variant="outline-dark" title="Github repo">
-                        <i className="fab fa-github-square"></i> source code-1
+                        <i className="fab fa-github-square"></i> source code
                       </Button><span>{"           "}</span>
-                      <Button variant="outline-dark" title="Github repo">
-                        <i className="fab fa-github-square"></i> source code-2
-                      </Button>
                     </a>
                   </Card.Footer>
                 </span> 
@@ -99,8 +96,13 @@ const TimeLine = () => {
                   <Card.Footer className="text-muted">
                     <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
                       <Button variant="outline-dark" title="Github repo">
-                        <i className="fab fa-github-square"></i> source code
+                        <i className="fab fa-github-square"></i> source code-1
                       </Button>
+                    </a>
+                    <a href={project.projectLinkTwo} target="_blank" rel="noopener noreferrer">
+                      <Button variant="outline-dark" title="Github repo">
+                        <i className="fab fa-github-square"></i> source code-2
+                      </Button><span>{"           "}</span>
                     </a>
                   </Card.Footer>
                 </span>

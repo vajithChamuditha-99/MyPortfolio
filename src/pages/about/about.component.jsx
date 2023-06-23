de64d5313b204b49ce5d816987ec4d755e28db1e
VajithC <vajithc@gmail.com>
2023-06-23 19:22:18 +0530

    Update About section

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -16,18 +16,36 @@ const About = () => {
           <Row className="pt-3 pb-5 align-items-center">
             <Col xs={12} md={6}>
               <Row className="justify-content-center mb-2 mr-2 ">
-                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
               </Row>
             </Col>
             <Col xs={12} md={6}>
               <Row className=" align-items-start p-2 my-details rounded">
                 Hi there! I am <strong>&nbsp;Vajith Chamuditha</strong>
-                
-                <br /> I am an undergraduate at Informatics Institute of Technology affiliated with the University of Westminster 
-                currently following a Software Engineering degree. As a student who is interested in full stack development,
-                 I look forward to any opportunity to improve my skills as a full stack developer.
-                 <br/>As a programmer, I've developed my analytical thinking and collaboration skills, and I love working with a team.
-                <br />I like to learn about new technologies, what problems are they solving and How can I use them to build better and scalable products.
+                <br /> As a fresh graduate of BEng Software Engineering (Hons),
+                I am an enthusiastic and versatile full stack engineer. With a
+                solid foundation in software development principles and a broad
+                range of technical skills, I excel in both front-end and
+                back-end development. I possess expertise in front-end
+                technologies like React, Angular and Vue, allowing me to create
+                engaging and user-friendly interfaces. Additionally, my
+                knowledge of back-end technologies such as databases,
+                server-side frameworks, and programming languages enables me to
+                handle complex data management and build robust and restful
+                APIs.
+                <br />I like to learn about new technologies, what problems are
+                they solving and How can I use them to build better and scalable
+                products.
+                <br />
+                Currently, I am actively seeking opportunities to apply my
+                skills and contribute to impactful projects as a full stack
+                engineer.
                 <br /> <br />
                 <Col className="d-flex justify-content-center flex-wrap">
                   <div>
@@ -38,21 +56,33 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1cGoYxHBbkxgJ-gMV_qVkUWIgRQpFtN5h/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://drive.google.com/file/d/1xzgyK9G0d1Tf4NeaH2aNdQ-sai26GmvT/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="https://github.com/vajithChamuditha-99" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://github.com/vajithChamuditha-99"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-dark">
                         GitHub
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="http://linkedin.com/in/vajith-chamuditha-9545b7195" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="http://linkedin.com/in/vajith-chamuditha-9545b7195"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-info">
                         LinkedIn
                       </Button>

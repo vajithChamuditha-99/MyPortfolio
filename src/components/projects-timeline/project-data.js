ebe2cca7ad12618b6ed67fb062d7c6e4b0c62b0a
VajithC <vajithc@gmail.com>
2023-06-23 18:22:51 +0530

    Projects section responsive issue fix

--- src/components/projects-timeline/project-data.js
+++ src/components/projects-timeline/project-data.js
@@ -11,7 +11,11 @@ export const projects = [
     projectDescrep:
       "CyberSuccor is an AI-powered Sinhala Cyber Bullying Detection website that uses NLP and DL to identify Cyberbullying content online. By allowing users to input statements or images, it quickly determines if they contain bullying phrases. This innovative tool empowers users to take control of their online experience, ensuring a safe and inclusive environment by identifying offensive or hurtful language. Furthermore, CyberSuccor goes the extra mile by providing recommendations and notifying trusted contacts when a user continuously checks for bullying phrases, strengthening its role in fostering a positive digital community.",
     projectLinks: [
-      { name: "Demo", link: "https://youtu.be/RZ9dHKT3s9M", type: "youtube" },
+      {
+        name: "Demo Video",
+        link: "https://youtu.be/RZ9dHKT3s9M",
+        type: "youtube",
+      },
     ],
   },
   {
@@ -20,7 +24,11 @@ export const projects = [
     projectDescrep:
       "BirdO is a new kind of bird ID tool created for the second year group project. When a user upload an image of an unrecognized bird to BirdO, it find out what bird it is that you most likely encountered in the vicinity of where you saw the bird. BirdO will provide major information about the bird you just encountered.",
     projectLinks: [
-      { name: "Demo", link: "https://youtu.be/lKleLLGqytc/", type: "youtube" },
+      {
+        name: "Demo Video",
+        link: "https://youtu.be/lKleLLGqytc/",
+        type: "youtube",
+      },
     ],
   },
   {
@@ -30,12 +38,12 @@ export const projects = [
       "A train ticket booking system with GUI developed using Java and MongoDB. Major functionalities: book a seat, view all seats, view empty seats, delete booked seats, find a booked seat using customer name, delete a reservation. Part 2: Boarding the customers to the train randomly. Generating a report of customer waitings.",
     projectLinks: [
       {
-        name: "GitHub",
+        name: "Code 1",
         link: "https://github.com/vajithChamuditha-99/Train-Booking-System-01",
         type: "github",
       },
       {
-        name: "Alternate GitHub",
+        name: "Code 2",
         link: "https://github.com/vajithChamuditha-99/Train-Booking-System-02",
         type: "github",
       },

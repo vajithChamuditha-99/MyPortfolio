d86089dad8b866a859307f1ae0fd31191e47c1fb
VajithC <vajithc@gmail.com>
2023-06-23 18:14:05 +0530

    UI UX fixes in projects section

--- src/components/projects-timeline/project-data.js
+++ src/components/projects-timeline/project-data.js
@@ -2,45 +2,69 @@ import PROJ_IMAGE_01 from "../../assets/img/projects/project-image-1.jpg";
 import PROJ_IMAGE_02 from "../../assets/img/projects/project-image-2.jpg";
 import PROJ_IMAGE_03 from "../../assets/img/projects/project-image-3.jpg";
 import PROJ_IMAGE_04 from "../../assets/img/projects/project-image-4.png";
+import PROJ_IMAGE_05 from "../../assets/img/projects/fyp.PNG";
 
-
-export const projects = {
-  projectOne: [
-    {
-      projectName: "BirdO - Bird Recognition App",
-      projectimgSrc: PROJ_IMAGE_01,
-      projectDescrep: "BirdO is a new kind of bird ID tool created for the University of Westminster BSc. CS/ BEng. SE second year group project by Team Aurora. When you upload an image of an unrecognized bird to BirdO, it find out what bird it is that you most likely encountered in the vicinity of where you saw the bird. BirdO will provide major information about the bird you just encountered.",
-      projectLink: "https://youtu.be/lKleLLGqytc/"
-    }
-    
-  ],
-
-  projectTwo: [
-    {
-        projectName: "Train Ticket Reservation System ",
-        projectimgSrc: PROJ_IMAGE_02,
-        projectDescrep: "A train ticket booking system with GUI developed using java and mongodb. Major functionalities; book a seat, view all seats, viee empty seats, delete booked seats, find a booked seat using customer name, delete a reservation. Part 2: Boarding the customers to the train by randomly. Generating a report of customer waitings.",
-        projectLink: "https://github.com/vajithChamuditha-99/Train-Booking-System-01",
-        projectLinkTwo: "https://github.com/vajithChamuditha-99/Train-Booking-System-02",
-    },
-  ],
-  projectThree: [
-    {
-        projectName: "Primier League Management System ",
-        projectimgSrc: PROJ_IMAGE_03,
-        projectDescrep: "A football premier league management system with GUI developed using java and javafx. All OOP concepts are used to develop the application. Matches table, Points table are available. Major functionalities: Random match generation , Search matches using date.",
-        projectLink: "https://github.com/vajithChamuditha-99/Primier-league-manager-java-javaFX",
-
-    },
-  ],
-  projectFour: [
-    {
-        projectName: "Crypto Tracker",
-        projectimgSrc: PROJ_IMAGE_04,
-        projectDescrep: "A crypto curruncy tracker app. Developed using react native expo and redux. Major functionalities: Track trending crypto currency with their cost and up and downs in last 24 hours and 7 days.",
-        projectLink: "https://github.com/vajithChamuditha-99/react-native-crypto-tracker" ,
-
-    },
-  ],
-
-};
+export const projects = [
+  {
+    projectName: "CyberSuccor - Final year project",
+    projectimgSrc: PROJ_IMAGE_05,
+    projectDescrep:
+      "CyberSuccor is an AI-powered Sinhala Cyber Bullying Detection website that uses NLP and DL to identify Cyberbullying content online. By allowing users to input statements or images, it quickly determines if they contain bullying phrases. This innovative tool empowers users to take control of their online experience, ensuring a safe and inclusive environment by identifying offensive or hurtful language. Furthermore, CyberSuccor goes the extra mile by providing recommendations and notifying trusted contacts when a user continuously checks for bullying phrases, strengthening its role in fostering a positive digital community.",
+    projectLinks: [
+      { name: "Demo", link: "https://youtu.be/RZ9dHKT3s9M", type: "youtube" },
+    ],
+  },
+  {
+    projectName: "BirdO - Bird Recognition App",
+    projectimgSrc: PROJ_IMAGE_01,
+    projectDescrep:
+      "BirdO is a new kind of bird ID tool created for the second year group project. When a user upload an image of an unrecognized bird to BirdO, it find out what bird it is that you most likely encountered in the vicinity of where you saw the bird. BirdO will provide major information about the bird you just encountered.",
+    projectLinks: [
+      { name: "Demo", link: "https://youtu.be/lKleLLGqytc/", type: "youtube" },
+    ],
+  },
+  {
+    projectName: "Train Ticket Reservation System",
+    projectimgSrc: PROJ_IMAGE_02,
+    projectDescrep:
+      "A train ticket booking system with GUI developed using Java and MongoDB. Major functionalities: book a seat, view all seats, view empty seats, delete booked seats, find a booked seat using customer name, delete a reservation. Part 2: Boarding the customers to the train randomly. Generating a report of customer waitings.",
+    projectLinks: [
+      {
+        name: "GitHub",
+        link: "https://github.com/vajithChamuditha-99/Train-Booking-System-01",
+        type: "github",
+      },
+      {
+        name: "Alternate GitHub",
+        link: "https://github.com/vajithChamuditha-99/Train-Booking-System-02",
+        type: "github",
+      },
+    ],
+  },
+  {
+    projectName: "Premier League Management System",
+    projectimgSrc: PROJ_IMAGE_03,
+    projectDescrep:
+      "A football premier league management system with GUI developed using Java and JavaFX. All OOP concepts are used to develop the application. Matches table and Points table are available. Major functionalities: Random match generation, Search matches using date.",
+    projectLinks: [
+      {
+        name: "GitHub",
+        link: "https://github.com/vajithChamuditha-99/Primier-league-manager-java-javaFX",
+        type: "github",
+      },
+    ],
+  },
+  // {
+  //   projectName: "Crypto Tracker",
+  //   projectimgSrc: PROJ_IMAGE_04,
+  //   projectDescrep:
+  //     "A cryptocurrency tracker app developed using React Native Expo and Redux. Major functionalities: Track trending cryptocurrencies with their cost and ups and downs in the last 24 hours and 7 days.",
+  //   projectLinks: [
+  //     {
+  //       name: "GitHub",
+  //       link: "https://github.com/vajithChamuditha-99/react-native-crypto-tracker",
+  //       type: "github",
+  //     },
+  //   ],
+  // },
+];
--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -8,114 +8,71 @@ import Col from "react-bootstrap/Col";
 import "./projects-timeline.styles.css";
 import { projects } from "./project-data";
 
-
 const TimeLine = () => {
   return (
     <div id="projects">
       <div id="pt-3 pb-3" className="section-add-1 grid-container pt-3 pb-3">
-        <h1 className="text-center font-details-b">PROJECTS</h1><br/><br/>
+        <h1 className="text-center font-details-b">PROJECTS</h1>
+        <br />
+        <br />
         <CardDeck>
           <Row className="d-flex justify-content-around">
-            <Col>
-              <Card className="text-center">
-                {projects.projectOne.map((project, index) => (
-                  <span className="p-2" key={index}>
+            {projects.map((project, index) => (
+              <Col key={index}>
+                <Card className="text-center project-card">
+                  <span className="p-2">
                     <Card.Title>{project.projectName}</Card.Title>
                     <Card.Body>
-                      <Image src={project.projectimgSrc} alt="project1-image" height="100%" width="100%"></Image>
-                      <Card.Text>
-                        <br/>
+                      <Image
+                        src={project.projectimgSrc}
+                        alt="project1-image"
+                        height="100%"
+                        width="100%"
+                      />
+                      <Card.Text className="project-text">
+                        <br />
                         {project.projectDescrep}
                       </Card.Text>
                     </Card.Body>
-                    <Card.Footer className="text-muted">
-                      <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
-                        <Button  id="youtube-btn" variant="outline-danger" title="watch the live project!">
-                          <i className="fab fa-youtube"></i> live project
-                        </Button>
-                      </a>
+                    <Card.Footer className="text-muted project-footer">
+                      {project.projectLinks.map((link, linkIndex) => (
+                        <a
+                          key={linkIndex}
+                          href={link.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <Button
+                            id={`${link.name.toLowerCase()}-btn`}
+                            variant={
+                              link.type === "youtube"
+                                ? "outline-danger"
+                                : "outline-dark"
+                            }
+                            title={`Watch the ${link.name} project!`}
+                          >
+                            <i
+                              className={`fab fa-${link.type.toLowerCase()}`}
+                            />{" "}
+                            {link.name}
+                          </Button>
+                        </a>
+                      ))}
                     </Card.Footer>
                   </span>
-                ))}
-              </Card>
-            </Col>
-            <Col>
-            <Card className="text-center">
-                {projects.projectFour.map((project, index) => (
-                  <span className="p-2" key={index}>
-                    <Card.Title>{project.projectName}</Card.Title>
-                    <Image src={project.projectimgSrc} alt="project2-image" rounded height="60%" width="55%" ></Image>
-                    <Card.Text>
-                      <br/>
-                      {project.projectDescrep}
-                    </Card.Text>
-                    <Card.Footer className="text-muted">
-                      <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
-                        <Button variant="outline-dark" title="Github repo">
-                          <i className="fab fa-github-square"></i> source code
-                        </Button><span>{"           "}</span>
-                      </a>
-                    </Card.Footer>
-                  </span> 
-                ))}
-              </Card>
-            </Col>
-          </Row>
-          <br/>
-          <Row>
-            <Col>
-            <Card className="text-center">
-                {projects.projectThree.map((project, index) => (
-                  <span className="p-2" key={index}>
-                    <Card.Title>{project.projectName}</Card.Title>
-                    <Image src={project.projectimgSrc} alt="project3-image" rounded height="84%" width="78%"></Image>
-                    <Card.Text>
-                      <br/>
-                      {project.projectDescrep}
-                    </Card.Text>
-                    <Card.Footer className="text-muted">
-                      <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
-                        <Button variant="outline-dark" title="Github repo">
-                          <i className="fab fa-github-square"></i> source code
-                        </Button>
-                      </a>
-                    </Card.Footer>
-                  </span>
-                ))}
-              </Card>
-            </Col>
-            <Col>
-            <Card className="text-center">
-                {projects.projectTwo.map((project, index) => (
-                  <span className="p-2" key={index}>
-                    <Card.Title>{project.projectName}</Card.Title>
-                    <Image src={project.projectimgSrc} alt="project4-image" height="100%" width="90%"></Image>
-                    <Card.Text>
-                      <br/>
-                      {project.projectDescrep}
-                    </Card.Text>
-                    <Card.Footer className="text-muted">
-                      <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
-                        <Button variant="outline-dark" title="Github repo">
-                          <i className="fab fa-github-square"></i> source code-1
-                        </Button>
-                      </a>
-                      <a href={project.projectLinkTwo} target="_blank" rel="noopener noreferrer">
-                        <Button variant="outline-dark" title="Github repo">
-                          <i className="fab fa-github-square"></i> source code-2
-                        </Button><span>{"           "}</span>
-                      </a>
-                    </Card.Footer>
-                  </span>
-                ))}
-              </Card>
-            </Col>
+                </Card>
+              </Col>
+            ))}
           </Row>
         </CardDeck>
-        <br/>
-        <br/>
+        <br />
+        <br />
         <footer>
-          <a href="https://github.com/vajithChamuditha-99/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/vajithChamuditha-99/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="gitbtn-00">
               <Button variant="primary" size="lg" block>
                 SEE MORE
@@ -129,5 +86,3 @@ const TimeLine = () => {
 };
 
 export default TimeLine;
-
-
